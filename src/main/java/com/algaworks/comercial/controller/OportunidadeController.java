1ae893e1f7aae4913de1c001c16831eedfe0b78f
lionani07 <lionani07@gmail.com>
2019-01-25 08:56:58 -0200

    add update, delete

--- src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -3,16 +3,22 @@ package com.algaworks.comercial.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.algaworks.comercial.model.Oportunidade;
 import com.algaworks.comercial.repository.OportunidadeRepository;
@@ -41,9 +47,38 @@ public class OportunidadeController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Oportunidade adicionar(@RequestBody Oportunidade oportunidade){
+	public Oportunidade adicionar(@Valid @RequestBody Oportunidade oportunidade){
+		Optional<Oportunidade> oportunidadeExistente = oportunidades
+				.findByDescricaoAndNomeProspecto(oportunidade.getDescricao(), oportunidade.getNomeProspecto());
+		if(oportunidadeExistente.isPresent()){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe uma oportunidade con o mesmo nombre e descriçao");
+		}
 		return oportunidades.save(oportunidade);
 	}
+	
+	
+	@PutMapping()
+	public Oportunidade update(@Valid @RequestBody Oportunidade oportunidade){
+		Optional<Oportunidade> oportunidadeExiste = oportunidades.findById(oportunidade.getId());
+		if(!oportunidadeExiste.isPresent()){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Oportunidade nao existe, nao pode ser atualizada");
+		}
+		return oportunidades.save(oportunidade);
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public void delele(@PathVariable Long id){
+		Optional<Oportunidade> oportunidadeExistente = oportunidades
+				.findById(id);
+		if(!oportunidadeExistente.isPresent()){
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Oportunidade nao existe");
+		}
+		oportunidades.deleteById(id);
+		
+	}
+	
+	
 	
 	
 
--- src/main/java/com/algaworks/comercial/repository/OportunidadeRepository.java
+++ src/main/java/com/algaworks/comercial/repository/OportunidadeRepository.java
@@ -1,5 +1,7 @@
 package com.algaworks.comercial.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.algaworks.comercial.model.Oportunidade;
 
 @Repository
 public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
+	
+	Optional<Oportunidade> findByDescricaoAndNomeProspecto(String descricao, String nomeProspecto);
 
 }
